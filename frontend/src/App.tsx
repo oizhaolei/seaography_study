d4fddf8755cfbafc1185019c1008897852ade0c1
lei <oizhaolei@gmail.com>
2025-01-25 11:42:15 +0900

    useQuery with variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,43 @@
 import { useQuery } from "@apollo/client";
-
-import { gql } from "./__generated__/gql";
 import { GetCakesDocument, GetCakesQuery } from "./__generated__/graphql";
 
-function DisplayLocations() {
-  const { loading, error, data } = useQuery<GetCakesQuery>(GetCakesDocument);
+function DisplayCakes() {
+  const { loading, error, data } = useQuery<GetCakesQuery>(GetCakesDocument, {
+    variables: { glutenFree: true },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error || !data) return <p>Error : {error?.message || "no data"}</p>;
 
-  console.log("data:", data);
-  return data.cake.nodes.map(({ id, name, price, glutenFree, bakery }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <p>{glutenFree}</p>
-      <p>{price}</p>
-      <p>{bakery?.name}</p>
-    </div>
-  ));
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Gluten Free</th>
+          <th>Price</th>
+          <th>Bakery</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.cake.nodes.map(({ id, name, price, glutenFree, bakery }) => (
+          <tr key={id}>
+            <td>{name}</td>
+            <td>{glutenFree ? "Y" : "N"}</td>
+            <td>{price}</td>
+            <td>{bakery?.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default function App() {
   return (
     <div>
       <h2>My first Apollo app with codegen 🚀</h2>
-      <DisplayLocations />
+      <DisplayCakes />
     </div>
   );
 }
